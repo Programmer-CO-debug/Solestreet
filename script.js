76c7bc5553da2672d1bd8f6e1cdae7d4f7cf54d6
Programmer-CO-debug <aayushdarekar08@gmail.com>
2026-09-24 23:58:27 +0530

    Add functions for cart, login, and contact submission

---
+++ script.js
@@ -0,0 +1,30 @@
+function addToCart(productName) {
+
+    alert(
+        productName +
+        " has been added to your cart!"
+    );
+
+}
+
+
+function loginUser(event) {
+
+    event.preventDefault();
+
+    alert(
+        "Demo login successful!"
+    );
+
+}
+
+
+function submitContact(event) {
+
+    event.preventDefault();
+
+    alert(
+        "Thank you! Your message has been submitted."
+    );
+
+}
